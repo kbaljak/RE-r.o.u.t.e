304d75819b84353f9b91616031b0af32536c3f0b
Ian Marković <ian@epicentar.com>
2026-01-19 22:09:56 +0100

    pushy

---
+++ REroute/Assets/Scripts/Utility/PrefabCopy_Editor.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PrefabCopy_Editor : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
